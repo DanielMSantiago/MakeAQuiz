2457c7babb6633a5b8ff285378df5327553b65a9
B31rut <dn.snt0325@gmail.com>
2023-04-16 12:54:38 -0400

    Finished basic frontend for html file with bootstrap. Created JS file to randomly pick question from test bank using jquery for functionality. Test bank imported from MakeQuiz.js.

--- TakeQuiz.js
+++ TakeQuiz.js
@@ -3,7 +3,8 @@ import { questions } from './MakeQuiz.js';
 $(document).ready(function() {
     for (i = 0; i <= questions.length; i--) {
         Math.floor(Math.random() * questions[i])
-        $(Questions).html(questions[i]);
+        questions[i] - 1;
+        $(".Questions").html(questions[i]);
     }
 
 });
