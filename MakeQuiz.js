d60533aab28816f58ab794cc8dcd9c1cf726c7a8
B31rut <dn.snt0325@gmail.com>
2023-04-19 00:03:17 -0400

    Added jQuery. Created a confirm button to have user confirm entries. Added a list to the array to make it easier to separate questions.

--- MakeQuiz.js
+++ MakeQuiz.js
@@ -1,32 +1,54 @@
-var question;
-var shortOrMultiple;
-var choiceOne;
-var choiceTwo;
-var choiceThree;
-var choiceFour;
+const questions = [{}];
 
+let ConfirmQuestion = () => {
+    var question = document.forms['create-question']['Question'].value;
+    var choiceOne = document.forms['create-question']['ChoiceOne'].value;
+    var choiceTwo = document.forms['create-question']['ChoiceTwo'].value;
+    var choiceThree = document.forms['create-question']['ChoiceThree'].value;
+    var choiceFour = document.forms['create-question']['ChoiceFour'].value;
 
+    var confirmation = confirm('Question: ' + question + '\n' +
+        'Choice One: ' + choiceOne + '\n' +
+        'Choice Two: ' + choiceTwo + '\n' +
+        'Choice Three: ' + choiceThree + '\n' +
+        'Choice Four: ' + choiceFour
+    );
 
-
-let StoreQuestions = () => {
-    const questions = [];
-
-    questions.push(question, shortOrMultiple, choiceOne, choiceTwo, choiceThree, choiceFour);
-}
-
-let GetQuestion = () => {
-    question = document.forms['create-question']['Question'].value;
-    shortOrMultiple = true;
-    if (shortOrMultiple = true) {
-        document.forms['create-question']['MultipleChoice'].value;
+    if (confirmation == true) {
+        alert('Question Stored');
+        return true;
     } else {
-        document.forms['create-question']['ShortAnswer'].value;
+        alert('Please make the necessary changes');
+        window.history.go();
     }
-    choiceOne = document.forms['create-question']['ChoiceOne'].value;
-    choiceTwo = document.forms['create-question']['ChoiceTwo'].value;
-    choiceThree = document.forms['create-question']['ChoiceThree'].value;
-    choiceFour = document.forms['create-question']['ChoiceFour'].value;
-
-    StoreQuestions();
-
 }
+$(document).ready(function() {
+    $("button[name='GetAndStore']").click(function() {
+        var question = document.forms['create-question']['Question'].value;
+        /*shortOrMultiple = true;
+        if (shortOrMultiple = true) {
+            document.forms['create-question']['MultipleChoice'].value;
+        } else {
+            document.forms['create-question']['ShortAnswer'].value;
+        }*/
+        var choiceOne = document.forms['create-question']['ChoiceOne'].value;
+        var choiceTwo = document.forms['create-question']['ChoiceTwo'].value;
+        var choiceThree = document.forms['create-question']['ChoiceThree'].value;
+        var choiceFour = document.forms['create-question']['ChoiceFour'].value;
+
+        if (!ConfirmQuestion()) return false;
+
+        else {
+            questions.push({
+                question,
+                shortOrMultiple,
+                choiceOne,
+                choiceTwo,
+                choiceThree,
+                choiceFour
+            });
+        }
+        alert(questions.length);
+    });
+
+});
